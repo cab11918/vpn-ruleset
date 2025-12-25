5d7bce2d238b9878a145b91f515fc36533df1d7c
cab11918 <906924612@qq.com>
2025-12-25 11:16:11 +0800

    fix

--- generators/gen_clash.py
+++ generators/gen_clash.py
@@ -22,18 +22,18 @@ def generate_clash(airport_file):
         if ':' in stripped and not stripped.startswith('-') and not stripped.startswith('#'):
             key = stripped.split(':')[0].strip()
             section_lines = []
-            base_indent = len(line) - len(stripped)
+            user_base_indent = len(line) - len(stripped)
             i += 1
             while i < len(user_lines):
                 next_line = user_lines[i]
                 next_stripped = next_line.lstrip()
                 if next_stripped and not next_stripped.startswith('#'):
                     next_indent = len(next_line) - len(next_stripped)
-                    if next_indent <= base_indent and ':' in next_stripped and not next_stripped.startswith('-'):
+                    if next_indent <= user_base_indent and ':' in next_stripped and not next_stripped.startswith('-'):
                         break
                 section_lines.append(next_line)
                 i += 1
-            user_sections[key] = section_lines
+            user_sections[key] = (section_lines, user_base_indent)
         else:
             i += 1
     
@@ -49,19 +49,26 @@ def generate_clash(airport_file):
         if ':' in stripped and not stripped.startswith('-') and not stripped.startswith('#'):
             key = stripped.split(':')[0].strip()
             if key in user_sections:
-                base_indent = len(line) - len(stripped)
+                airport_base_indent = len(line) - len(stripped)
                 # 跳过机场section内容
                 while i < len(airport_lines):
                     next_line = airport_lines[i]
                     next_stripped = next_line.lstrip()
                     if next_stripped and not next_stripped.startswith('#'):
                         next_indent = len(next_line) - len(next_stripped)
-                        if next_indent <= base_indent and ':' in next_stripped and not next_stripped.startswith('-'):
+                        if next_indent <= airport_base_indent and ':' in next_stripped and not next_stripped.startswith('-'):
                             break
                     result_lines.append(next_line)
                     i += 1
-                # 追加用户内容
-                result_lines.extend(user_sections[key])
+                # 追加用户内容，调整缩进
+                section_lines, user_base_indent = user_sections[key]
+                indent_diff = airport_base_indent - user_base_indent
+                for user_line in section_lines:
+                    if user_line.strip():
+                        adjusted_line = ' ' * indent_diff + user_line if indent_diff > 0 else user_line[-indent_diff:] if indent_diff < 0 else user_line
+                        result_lines.append(adjusted_line)
+                    else:
+                        result_lines.append(user_line)
     
     with open('output/clash.yaml', 'w') as f:
         f.write('\n'.join(result_lines))
