0baded479d9743164d55cae55e25039223ac7300
cab11918 <906924612@qq.com>
2025-12-25 11:11:02 +0800

    fix

--- generators/gen_clash.py
+++ generators/gen_clash.py
@@ -44,12 +44,13 @@ def generate_clash(airport_file):
         line = airport_lines[i]
         stripped = line.lstrip()
         result_lines.append(line)
+        i += 1
         
         if ':' in stripped and not stripped.startswith('-') and not stripped.startswith('#'):
             key = stripped.split(':')[0].strip()
             if key in user_sections:
                 base_indent = len(line) - len(stripped)
-                i += 1
+                # 跳过机场section内容
                 while i < len(airport_lines):
                     next_line = airport_lines[i]
                     next_stripped = next_line.lstrip()
@@ -59,9 +60,8 @@ def generate_clash(airport_file):
                             break
                     result_lines.append(next_line)
                     i += 1
+                # 追加用户内容
                 result_lines.extend(user_sections[key])
-                continue
-        i += 1
     
     with open('output/clash.yaml', 'w') as f:
         f.write('\n'.join(result_lines))
